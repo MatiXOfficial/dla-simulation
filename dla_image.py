f61ba2f44a6298bd083fb9261c705a468ffa6d27
ChrisFlam3 <krzysmisan@gmail.com>
2022-11-29 22:41:19 +0100

    Attractor field - initial implementation

--- dla_image.py
+++ dla_image.py
@@ -1,4 +1,6 @@
 import random
+import time
+import math
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -6,17 +8,95 @@ from matplotlib import pyplot as plt
 from config import Config, InitType
 
 
+def length(p1,p2):
+    vec=(abs(p1[0]-p2[0]),abs(p1[1]-p2[1]))
+    return math.sqrt(vec[0]*vec[0]+vec[1]*vec[1])
+
+
+class SphereAttractor:
+    def __init__(self,pos,radius,force,negative=False):
+        self.pos=pos
+        self.radius=radius
+        self.negative=negative
+        self.force=force
+
+    def check(self, pos):
+        distance = length(self.pos, pos) - self.radius
+        if distance<0.:
+            return False
+        return True
+
+    def get_probability(self,pos):
+        distance=length(self.pos,pos) - self.radius
+        weight=[]
+        for move in DLAImage.possible_moves:
+            current_pos=pos+move
+            current_distance=length(self.pos,current_pos) - self.radius
+            default_probability = 1. / len(DLAImage.possible_moves)
+
+            if current_distance<=1.0:
+                weight.append((current_distance-distance)*(1./(abs(current_distance)+0.001)) * 100.0+default_probability)
+                continue
+
+            if self.negative==False:
+                weight.append((distance-current_distance)*(1./current_distance) * self.force+default_probability)
+            else:
+                weight.append((current_distance-distance)*(1./current_distance) * self.force+default_probability)
+
+        weight=np.array(weight)
+        min=np.min(weight)
+        weight+=abs(min)
+        sum=np.sum(weight)
+        # if sum != 0:
+        weight/=sum
+        # else:
+        #     weight+=default_probability
+
+        return weight
+
+class AttractorField:
+    attractorList=[]
+
+    def add_attractor(self, attractor):
+        self.attractorList.append(attractor)
+
+    def get_weights(self,pos):
+        if len(self.attractorList)==0:
+            return [1./len(DLAImage.possible_moves)]*len(DLAImage.possible_moves)
+        weights=np.zeros(len(DLAImage.possible_moves))
+
+        for attractor in self.attractorList:
+            weights+=attractor.get_probability(pos)
+        weights/=len(self.attractorList)
+
+        return list(weights)
+
+    def check(self,pos):
+        for attractor in self.attractorList:
+            if attractor.check(pos)==False:
+                return False
+        return True
+
+
 class DLAImage:
+    possible_moves = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
     PERIOD = 10
 
     def __init__(self, config: Config):
         self.config = config
 
+        self.grid: set = None
+        self.grid_len: int = None
+        self.particles: np.ndarray = None
+        self.attractorField=AttractorField()
+
+    def init(self):
         self.grid = set()
         self.initialize_grid()
 
         self.grid_len = len(self.grid)
         self.particles = np.array([self._random_position()])
+        self.attractorField.add_attractor(SphereAttractor((5,5),5,10,False))
 
     def initialize_grid(self):
         if self.config.init_type == InitType.MIDDLE:
@@ -27,7 +107,7 @@ class DLAImage:
     def simulate_step(self):
         growth = []
         for i, particle in enumerate(self.particles):
-            move = self._random_move()
+            move = self._random_move(particle)
 
             new_position = particle + move
 
@@ -40,6 +120,8 @@ class DLAImage:
 
             if self._is_valid_pos(new_position):
                 self.particles[i] = new_position
+            # if self.attractorField.check(new_position)==False:
+            #     self.particles[i] = self._random_position()
 
         return growth
 
@@ -66,12 +148,13 @@ class DLAImage:
         while True:
             pos = np.random.randint(low=0, high=self.config.canvas_size, size=2)
             pos = (pos[0], pos[1])
-            if pos not in self.grid:
+            if pos not in self.grid and self.attractorField.check(pos):
                 return pos
 
-    @staticmethod
-    def _random_move():
-        return random.choice([(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)])
+
+    def _random_move(self,pos):
+        w=self.attractorField.get_weights(pos)
+        return random.choices(DLAImage.possible_moves,weights=w)[0]
 
     def _is_valid_pos(self, pos):
         return 0 <= pos[0] < self.config.canvas_size and 0 <= pos[1] < self.config.canvas_size
